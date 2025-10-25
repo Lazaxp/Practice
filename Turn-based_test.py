4b6bd260a814e8b8058f335e8f9ccc6da3e377de
Lazaxp <lasttsutz@gmail.com>
2025-10-25 11:17:51 +0800

    Refactor character and enemy data structures

--- Turn-based_test.py
+++ Turn-based_test.py
@@ -1,27 +1,15 @@
 #Quick game practice
 import random
-charas = ["Knight","Mage","Javelin"]
-enemies = ["Local Criminal","Hobgoblin","Demon"]
-def local_criminal():
-  hp1= 40
-  return hp1
-def knight():
-  hp = 100
-  basic_attack = 5
-  skill1 = 10
-  heal = 20
-  ultimate = 30
-  crit = 100
-  return hp, basic_attack, skill1, heal, ultimate, crit
+charas = [["Knight", 5 , 10 , 30 , 30, 100],["Mage", 5 , 25 , 20 , 30, 80],["Javelin", 5 , 15 , 25 , 20, 80]]
+enemies = [["Local Criminal", 100],["Hobgoblin" , 200],["Demon" , 500]]
 a = input("Welcome To My Test Turn-Based Game.\nPress Enter to Start.")
-clear_screen()
 def character_select():
   character = eval(input("Select your character(Type the number of your choice):\n(1) Knight - Big Base Attack\n(2) Mage - Big Base Magic and Crit\n(3) Javelin - Big Crit Rate\n\nEnter Here:\t"))
   character -= 1
   while True:
-    confirm = input(f"You've Selected {charas[character]}. Is this the Character you want? (Yes/No):\t").lower()
+    confirm = input(f"You've Selected {charas[character][0]}. Is this the Character you want? (Yes/No):\t").lower()
     if confirm == "yes":
-       print(f"{charas[character]} Confirmed.")
+       print(f"{charas[character][0]} Confirmed.")
        return character      
        break
     elif confirm == "no":
@@ -29,10 +17,10 @@ def character_select():
     else:
        print("Please Choose within the option.")
 def boss_select(character):
-  selection = eval(input(f"Current Character: {charas[character]}.\nSTAGE SELECT(Type the number of your choice)\n(1) - {enemies[0]}\n(2) - {enemies[1]}\n(3) - {enemies[2]}\nEnter Here:\t"))
+  selection = eval(input(f"Current Character: {charas[character][0]}.\nSTAGE SELECT(Type the number of your choice)\n(1) - {enemies[0][0]}\n(2) - {enemies[1][0]}\n(3) - {enemies[2][0]}\nEnter Here:\t"))
   selection -= 1  
   while True:
-    confirm = input(f"You've selected to battle with {enemies[selection]}. Are you sure? (Yes or No):\t")
+    confirm = input(f"You've selected to battle with {enemies[selection][0]}. Are you sure? (Yes or No):\t")
     if confirm == "yes":
       print("Confirmed.")
       return selection
@@ -42,30 +30,36 @@ def boss_select(character):
   else:
     print("Please choose within the options.")
 def battle(character, selection):  
-  if charas[character] == "Knight" and enemies[selection] == "Local Criminal":
-    hp1 = local_criminal()
-    hp, basic_attack, skill1, heal, ultimate, crit = knight()
     print("Battle Start")
     while True:
-      print(f"Enemy {enemies[selection]}\n\nCurrent HP:{hp1}")
-      turn = input(f"What would you like to do?:\n(1)Basic Attack - {basic_attack}DMG\t(2)Skill - {skill1}DMG\n(3)Heal Self - {heal}HP\t(4)Ultimate {ultimate}DMG\nEnter here:\t")
+      print(f"Enemy {enemies[selection][0]}\n\n{enemies[selection][0]} Current HP:{enemies[selection][1]}\nYour Current HP: {charas[character][5]}")
+      turn = input(f"What would you like to do?:\n(1)Basic Attack - {charas[character][1]}DMG\t(2)Skill - {charas[character][2]}DMG\n(3)Heal Self - {charas[character][3]}HP\t(4)Ultimate {charas[character][4]}DMG\nEnter here:\t")
       if turn == "1":
-        hp1 -= basic_attack
-        print(f"Dealt {basic_attack} to {enemies[selection]}")
+        enemies[selection][1] -= charas[character][1]
+        print(f"Dealt Basic Attack(-{charas[character][1]}) to {enemies[selection][0]}")
       elif turn == "2":
-        hp1 -= skill1
-        print(f"Dealt {skill1} to {enemies[selection]}")
+        enemies[selection][1] -= charas[character][2]
+        print(f"Dealt skill(-{charas[character][2]}) to {enemies[selection][0]}")
       elif turn == "3":
-        hp += heal
-        print(f"Heals self by {heal}")
+        enemies[selection][1] += charas[character][3]
+        print(f"Heals self by {charas[character][3]}")
       elif turn == "4":
-        hp1 -= ultimate
-        print(f"Dealt {ultimate} to {enemies[selection]}")
-      else:
+        enemies[selection][1] -= charas[character][4]
+        print(f"Dealt Ultimate (-{charas[character][4]}) to {enemies[selection][0]}")
+      else: 
         print("Choose a Valid Option.")
-      if hp1 <= 0:
+      if enemies[selection][1] <= 0:
         print("Enemy Defeated")
         break
+      elif charas[character][5] <= 0:
+        print("You have been defeated")
+        r = input("Try again?:\t").lower()
+        if r == yes:
+          return battle()
+        elif r == no:
+          break
+        else:
+          print("Choose within the options")
 character = character_select()
 selection = boss_select(character)
 battle(character, selection)
