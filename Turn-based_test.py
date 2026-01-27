a34035ff6c0848e16228fd0c553b03bcd06f0def
Lazaxp <lasttsutz@gmail.com>
2026-01-27 18:46:28 +0800

    Add 'Random' stage option and adjust delays

--- Turn-based_test.py
+++ Turn-based_test.py
@@ -39,7 +39,7 @@ def character_select():
 def boss_select(character):
   while True:
     os.system("cls")
-    print(f"\nCurrent Character: {character}\n\nSTAGE SELECT:\n\nLocal_Criminal - Easy\nHobgoblin - Medium\nDemon - Hard\n")
+    print(f"\nCurrent Character: {character}\n\nSTAGE SELECT:\n\nLocal_Criminal - Easy\nHobgoblin - Medium\nDemon - Hard\nRandom - Random")
     selection = input("Enter Here:\t").capitalize()
     if selection in enemies:
       confirm = input(f"You've selected to battle with {selection}. Are you sure? (Yes or No):\t")
@@ -53,7 +53,13 @@ def boss_select(character):
       else:
         os.system("cls")
         print("Please choose within the options.")
-        
+    elif selection == "Random":
+      e = random.choice(list(enemies.keys()))
+      selection = e
+      cls(-0.5)
+      print(f"You've been set to battle with {selection}.")
+      cls(1)
+      return selection
     else:
       os.system("cls")
       print("\n\nInvalid Option.\n\n")
@@ -64,9 +70,9 @@ def battle(character, selection, action, dmg, crithit):
   while True:
     cls(1.1)
     print(f"Enemy {selection}\n\n{selection} Current HP: {enemies.get(selection).get('HP')}\nYour Current HP: {charas.get(character).get('HP')}")
-    turn = input(f"\nWhat would you like to do?:\n\n(1)Basic Attack - {charas.get(character).get('Basic_attack')}DMG - AP: {ability_points['ba']}\t(2)Skill - {charas.get(character).get('Skill')}DMG - AP: {ability_points['skill']}\n(3)Heal Self - {charas.get(character).get('heal')}HP - AP: {ability_points['heal']}'\t(4)Ultimate {charas.get(character).get('ultimate')}DMG - AP: {ability_points['ulti']}\nEnter here:\t")
+    turn = input(f"\nWhat would you like to do?:\n\n(1)Basic Attack - {charas.get(character).get('Basic_attack')}DMG - AP: {ability_points['ba']}\t(2)Skill - {charas.get(character).get('Skill')}DMG - AP: {ability_points['skill']}\n(3)Heal Self - {charas.get(character).get('heal')}HP - AP: {ability_points['heal']}\t(4)Ultimate {charas.get(character).get('ultimate')}DMG - AP: {ability_points['ulti']}\nEnter here:\t")
     if turn == "1": 
-      cls(0)      
+      cls(0.5)      
       enemies[selection]["HP"] -= charas[character]["Basic_attack"]
       if crithit == True:        
         enemies[selection]["HP"] -= charas[character]["crit"]
@@ -75,7 +81,7 @@ def battle(character, selection, action, dmg, crithit):
         print("\n{selection} used {action}! Dealt {enemies[selection][action]} to {character} HP:{charas[character]['HP']}\n\n")
       else:
         print(f"\n\nDealt Basic Attack (-{charas[character]['Skill']}) to {selection}\n")
-        time.sleep(0.5)
+        time.sleep(0.8)
         print(f"\n{selection} used {action}! Dealt {enemies[selection][action]} to {character} HP:{charas[character]['HP']}\n\n")
       charas[character]["HP"] -= dmg
     elif turn == "2":
@@ -87,11 +93,11 @@ def battle(character, selection, action, dmg, crithit):
       elif crithit == True:
         enemies[selection]["HP"] -= charas[character]["crit"]
         print(f"\n\nCritical Hit! Dealt Skill (-{charas[character]['Skill']+charas[character]['crit']}) to {selection}\n")
-        time.sleep(0.5)
+        time.sleep(0.8)
         print(f"\n{selection} used {action}! Dealt {enemies[selection][action]} to {character} HP:{charas[character]['HP']}\n\n")
       else:
         print(f"\n\nDealt Skill (-{charas[character]['Basic_attack']}) to {selection}\n")
-        time.sleep(0.5)
+        time.sleep(0.8)
         print(f"\n{selection} used {action}! Dealt {enemies[selection][action]} to {character} HP:{charas[character]['HP']}\n\n")
       charas[character]["HP"] -= dmg
       ability_points["skill"] -= 1
@@ -104,7 +110,7 @@ def battle(character, selection, action, dmg, crithit):
         if charas[character]["HP"] > 100:
           charas[character]["HP"] = 100
         print(f"\n\nHeals self by {charas[character]['heal']}\n")
-        time.sleep(0.5)
+        time.sleep(0.8)
         print(f"\n{selection} used {action}! Dealt {enemies[selection][action]}\n\n")
       charas[character]["HP"] -= dmg
       ability_points["heal"] -= 1
@@ -117,11 +123,11 @@ def battle(character, selection, action, dmg, crithit):
       elif crithit == True:
         enemies[selection]["HP"] -= charas[character]["crit"]
         print(f"\n\nCritical Hit! Dealt Ultimate(-{charas[character]['ultimate']+charas[character]['crit']}) to {selection}\n")
-        time.sleep(0.5)
+        time.sleep(0.8)
         print(f"\n{selection} used {action}! Dealt {enemies[selection][action]} to {character} HP:{charas[character]['HP']}\n\n")
       else:
         print(f"\n\nDealt Ultimate(-{charas[character]['ultimate']}) to {selection}\n")
-        time.sleep(0.5)
+        time.sleep(0.8)
         print(f"\n{selection} used {action}! Dealt {enemies[selection][action]} to {character} HP:{charas[character]['HP']}\n\n")
         charas[character]["HP"] -= dmg   
         ability_points["ulti"] -= 1   
@@ -137,17 +143,18 @@ def battle(character, selection, action, dmg, crithit):
       print("You have been defeated")
       r = input("Try again?:\t").lower()
       if r == "yes":
-        return battle()
+        return battle(character, selection, action, dmg, crithit)
       elif r == "no":
         break
       else:
         print("Choose within the options")
       cls(0)
+
 def enemy_behaviour(selection):
   if random.randint(1,25) == 1:
     action = "Ultimate"
     dmg = enemies[selection]["Ultimate"]
-    return action, dmg
+    return action, dmg (f"The {selection} used its Ultimate Attack!")
   else: 
     if random.randint (1,3) != 2:
       action = "Basic Attack"
@@ -167,10 +174,12 @@ def critical(character):
   else:
     crithit = False
     return crithit
+  
 character = character_select()
 selection = boss_select(character)
 action, dmg = enemy_behaviour(selection)
 crithit = critical(character)
 count = battle(character, selection, action, dmg, crithit) 
+
 
 
